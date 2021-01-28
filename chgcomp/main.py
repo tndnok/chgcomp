40d25b00856776c4dc5192fafadf0700cb1f4a40
tndnok <tsunoda.n.aa@m.titech.ac.jp>
2021-01-28 10:01:06 +0900

    Add slice mode

--- chgcomp/main.py
+++ chgcomp/main.py
@@ -4,10 +4,10 @@
 import argparse
 import sys
 
-from pymatgen import Structure
 from pymatgen.io.vasp import Chgcar
 
-from chgcomp.main_function import freeze_isosurface, unpack_isosurface
+from chgcomp.main_function import compress_isosurface, decompress_isosurface, \
+    compress_isosurfaces, decompress_isosurfaces
 
 
 def parse_args(args):
@@ -16,29 +16,55 @@ def parse_args(args):
 
     subparsers = parser.add_subparsers()
 
-    # -- freeze_isosurface -------------------------------
-    parser_freeze_isosurface = subparsers.add_parser(
-        name="freeze_isosurface",
-        description="",
+    # -- compress_isosurface -------------------------------
+    parser_compress_isosurface = subparsers.add_parser(
+        name="compress_isosurface",
+        description="compress CHGCAR to a single isosurface",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        aliases=['fi'])
+        aliases=['ci'])
 
-    parser_freeze_isosurface.add_argument("-c", "--chgcar", required=True,
-                                          type=Chgcar.from_file)
-    parser_freeze_isosurface.add_argument("-l", "--level", default=0.5,
-                                          type=float)
-    parser_freeze_isosurface.set_defaults(func=freeze_isosurface)
+    parser_compress_isosurface.add_argument("-c", "--chgcar", required=True,
+                                            type=Chgcar.from_file)
+    parser_compress_isosurface.add_argument("-l", "--level", default=0.5,
+                                            type=float)
+    parser_compress_isosurface.set_defaults(func=compress_isosurface)
 
-    # -- unpack_isosurface -------------------------------
-    parser_unpack_isosurface = subparsers.add_parser(
-        name="unpack_isosurface",
-        description="",
+    # -- decompress_isosurface -------------------------------
+    parser_decompress_isosurface = subparsers.add_parser(
+        name="decompress_isosurface",
+        description="decompress a single isosurface",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        aliases=['ui'])
+        aliases=['di'])
 
-    parser_unpack_isosurface.add_argument("-i", "--isurf", required=True,
-                                          type=str)
-    parser_unpack_isosurface.set_defaults(func=unpack_isosurface)
+    parser_decompress_isosurface.add_argument("-i", "--isurf", required=True,
+                                              type=str)
+    parser_decompress_isosurface.add_argument("-s", "--suffix", default=None,
+                                              help="suffix after output CHGCAR.")
+    parser_decompress_isosurface.set_defaults(func=decompress_isosurface)
+
+    # -- compress_isosurfaces -------------------------------
+    parser_compress_isosurfaces = subparsers.add_parser(
+        name="compress_isosurfaces",
+        description="compress a CHGCAR to 7 silces.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        aliases=['cis'])
+
+    parser_compress_isosurfaces.add_argument("-c", "--chgcar", required=True,
+                                             type=Chgcar.from_file)
+    # parser_compress_isosurfaces.add_argument("-s", "--slice", default=8, type=int,
+    #                                        help="8 or 16")
+    parser_compress_isosurfaces.set_defaults(func=compress_isosurfaces)
+
+    # -- decompress_isosurfaces -------------------------------
+    parser_decompress_isosurfaces = subparsers.add_parser(
+        name="decompress_isosurfaces",
+        description="decompress slices to 7 CHGCARs",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        aliases=['dis'])
+
+    parser_decompress_isosurfaces.add_argument("-i", "--isurf", required=True,
+                                               type=str)
+    parser_decompress_isosurfaces.set_defaults(func=decompress_isosurfaces)
 
     return parser.parse_args(args)
 
--- chgcomp/main_function.py
+++ chgcomp/main_function.py
@@ -2,10 +2,12 @@
 #  Copyright (c) 2020 Kumagai group.
 
 import pickle
+import numpy as np
+
 from pymatgen.io.vasp import Chgcar
 
 
-def freeze_isosurface(args):
+def compress_isosurface(args):
     c: Chgcar = args.chgcar
     raw_data = c.data["total"]
     ngx, ngy, ngz = [bin(g)[2:].zfill(10) for g in c.dim]
@@ -16,21 +18,63 @@ def freeze_isosurface(args):
     bin_stream = int(stream, 2)
 
     with open(f'isosurface_{args.level}.pickle', 'wb') as f:
-        pickle.dump((c.structure, bin_stream), f, protocol=4)
+        pickle.dump((c.structure, bin_stream, args.level), f, protocol=4)
 
 
-def unpack_isosurface(args):
+def decompress_isosurface(args):
     with open(f'{args.isurf}', 'rb') as f:
-        s, b = pickle.load(f)
+        s, b, level = pickle.load(f)
 
-    print(s.to("POSCAR"))
     bin_stream = bin(b)
     stream, ngx, ngy, ngz = bin_stream[2:-30], bin_stream[-30:-20], bin_stream[-20:-10], bin_stream[-10:]
-    print(int(ngx, 2), int(ngy, 2), int(ngz, 2))
     ngrid = int(ngx, 2)*int(ngy, 2)*int(ngz, 2)
     true_stream = stream.zfill(ngrid)
-    result = ""
+    result = s.to("POSCAR") + f"{int(ngx, 2)} {int(ngy, 2)} {int(ngz, 2)} \n"
     for bit in true_stream:
         result += bit
         result += " "
-    print(result)
+    suffix = args.suffix
+    if suffix is None:
+        suffix = int(float(args.isurf[11:-7]) * 1000)
+    _write_file(result, f"CHGCAR_{suffix}")
+
+
+def compress_isosurfaces(args):
+    c: Chgcar = args.chgcar
+    raw_data = c.data["total"]
+    c_max = c.data['total'].max()
+    levels = np.linspace(0, 1, 8, endpoint=False)[1:]
+    x_array = np.array([0]*c.ngridpts)
+    for level in levels:
+        mask = raw_data > float(level * c_max)
+        x_array += mask.astype(int).T.flatten()
+
+    x_stream = "".join(x_array.astype(str))
+    bin_stream = int(x_stream, 8)
+
+    with open(f'isosurface_slices.pickle', 'wb') as f:
+        pickle.dump((c.structure, c.dim, bin_stream),
+                    f, protocol=4)
+
+
+def decompress_isosurfaces(args):
+    with open(f'{args.isurf}', 'rb') as f:
+        s, dim, x = pickle.load(f)
+
+    header = s.to("POSCAR") + f"{dim[0]} {dim[1]} {dim[2]}" + "\n"
+    x_stream = oct(x)[2:]
+    true_x_stream = x_stream.zfill(dim[0]*dim[1]*dim[2])
+
+    levels = np.linspace(0, 1, 8, endpoint=False)[1:]
+    for n, level in enumerate(levels):
+        result = ""
+        for x in true_x_stream:
+            result += str(int(int(x) >= n+1))
+            result += " "
+        a_file = header + result
+        _write_file(a_file, f"CHGCAR_{int(level*1000)}")
+
+
+def _write_file(content, filename: str):
+    with open(filename, 'w') as f:
+        f.write(content)
